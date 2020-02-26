8cf7e7e08f18fe4d9e724b2d8ece72f8a7758ed2
Aritra Banerjee <aritraWorkBanerjee@gmail.com>
2020-02-26 17:50:00 +0530

    Added the phone validation in javascript

--- BLOG/authentication/register.js
+++ BLOG/authentication/register.js
@@ -3,3 +3,10 @@ var phone = document.getElementById("mobile_number");
 phone.onkeypress = function(){
     return ((event.charCode > 48 && event.charCode <= 57) ||  event.charCode == 43)
  }
+
+ phone.onblur = function() {
+    var phone_val= document.getElementById("mobile_number").value;
+    if(phone_val.length < 10 || phone_val.length > 14) {
+       alert("Invalid Phone number");
+    }
+ }
