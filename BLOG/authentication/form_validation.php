3c54f13c5bcc97e7b364cb4a9769d25ebafd0833
Aritra Banerjee <aritraWorkBanerjee@gmail.com>
2020-03-02 15:18:31 +0530

    Fixed the bugs showing in form validation

--- BLOG/authentication/form_validation.php
+++ BLOG/authentication/form_validation.php
@@ -1,5 +1,60 @@
 <?php
 
+$password = "";
 
-print_r($_POST);
-exit;
+if(isset($_POST['firstname'])) {
+    if(empty($_POST['firstname'])){
+        echo "false";
+    } else {
+        echo "true";
+    }
+}
+
+if(isset($_POST['lastname'])) {
+    if(empty($_POST['lastname'])){
+        echo "false";
+    } else {
+        echo "true";
+    }
+}
+
+if(isset($_POST['password'])) {
+    if(empty($_POST['password'])){
+        echo "false";
+    } else {
+        $password = $_POST['password'];
+        echo "true";
+    }
+}
+
+if(isset($_POST['check_password'])) {
+    if(!empty($_POST['check_password']) && $_POST['password'] == $_POST['check_password']){
+        echo "true";
+    } else {
+        echo "false";
+    }
+}
+
+if(isset($_POST['email'])) {
+    if(empty($_POST['email'])){
+        echo "false";
+    } else {
+        echo "true";
+    }
+}
+
+if(isset($_POST['mobilenumber'])) {
+    if(empty($_POST['mobilenumber'])){
+        echo "false";
+    } else {
+        echo "true";
+    }
+}
+
+if(isset($_POST['address'])) {
+    if(empty($_POST['address'])){
+        echo "false";
+    } else {
+        echo "true";
+    }
+}
--- BLOG/authentication/register.js
+++ BLOG/authentication/register.js
@@ -1,12 +1,14 @@
-var phone = document.getElementById("mobile_number");
+var phone = document.getElementById("mobilenumber");
 var userform = document.getElementById("userform");
+
+var error = [];
 
 phone.onkeypress = function(){
     return ((event.charCode > 48 && event.charCode <= 57) ||  event.charCode == 43)
  }
 
  phone.onblur = function() {
-    var phone_val= document.getElementById("mobile_number").value;
+    var phone_val= document.getElementById("mobilenumber").value;
     if(phone_val.length < 10 || phone_val.length > 14) {
        alert("Invalid Phone number");
     }
@@ -14,20 +16,153 @@ phone.onkeypress = function(){
 
  $(document).ready(function(){
 
-   $('#submit').attr("disabled", true);
-
-
- $('#firstname').onblur(function(){
+$("#reenter_password").attr("disabled", "true");
+   
+$('#firstname').blur(function(){
   var firstname = $('#firstname').val();
   $.ajax({
    type: 'POST',
-   data: firstname,
+   url : 'form_validation.php',
+   data: {"firstname" : firstname},
    success: function(response){
-   alert(response);
+      if(response == "true")
+      {
+         $('#firstname').css('border', '5px solid green');
+         $('#submit').attr("disabled", false);
+      } else {
+         $('#firstname').css('border', '5px solid red');
+         $('#submit').attr("disabled", true);
+      }
    }
   });
 
- });
+});
 
+ $('#lastname').blur(function(){
+   var lastname = $('#lastname').val();
+   $.ajax({
+    type: 'POST',
+    url : 'form_validation.php',
+    data: {"lastname" : lastname},
+    success: function(response){
+       if(response == "true")
+       {
+         $('#lastname').css('border', '5px solid green');
+         $('#submit').attr("disabled", false);
+       } else {
+         $('#lastname').css('border', '5px solid red')
+         $('#submit').attr("disabled", true);
+       }
+    }
+   });
+ 
+});
 
+$('#password').blur(function(){
+   var password = $('#password').val();
+   $.ajax({
+    type: 'POST',
+    url : 'form_validation.php',
+    data: {"password" : password},
+    success: function(response){
+       if(response == "true")
+       {
+         $('#password').css('border', '5px solid green');
+         $('#submit').attr("disabled", false);
+         $("#reenter_password").attr("disabled", false);
+       } else {
+         $('#password').css('border', '5px solid red')
+         $('#submit').attr("disabled", true);
+       }
+    }
+   });
+ 
+});
+
+$('#reenter_password').blur(function(){
+   var check_password = $('#reenter_password').val();
+   var password = $("#password").val();
+   $.ajax({
+    type: 'POST',
+    url : 'form_validation.php',
+    data: {"password" : password,
+          "check_password" : check_password},
+    success: function(res){
+       if(res == "truetrue")
+       {
+         $('#reenter_password').css('border', '5px solid green');
+         $('#submit').attr("disabled", false);
+       } else {
+         $('#reenter_password').css('border', '5px solid red')
+         $('#submit').attr("disabled", true);
+         console.log(res);
+       }
+    }
+   });
+ 
+  });
+
+  $('#email').blur(function(){
+   var email = $('#email').val();
+   $.ajax({
+    type: 'POST',
+    url : 'form_validation.php',
+    data: {"email" : email},
+    success: function(response){
+       if(response == "true")
+       {
+         $('#email').css('border', '5px solid green')
+         $('#submit').attr("disabled", false);
+       } else {
+         $('#email').css('border', '5px solid red')
+         $('#submit').attr("disabled", true);
+       }
+    }
+   });
+});
+   
+
+   $('#mobilenumber').blur(function(){
+      var mobile = $('#mobilenumber').val();
+      $.ajax({
+       type: 'POST',
+       url : 'form_validation.php',
+       data: {"mobilenumber" : mobile},
+       success: function(response){
+          if(response == "true")
+          {
+            $('#mobilenumber').css('border', '5px solid green')
+            $('#submit').attr("disabled", false);
+          } else {
+            $('#mobilenumber').css('border', '5px solid red')
+            $('#submit').attr("disabled", true);
+            console.log(response);
+          }
+       },
+       error : (e) => {
+         alert("error");
+       }
+      });
+    
+     });
+
+  $('#address').blur(function(){
+   var address = $('#address').val();
+   $.ajax({
+    type: 'POST',
+    url : 'form_validation.php',
+    data: {"address" : address},
+    success: function(response){
+       if(response == "true")
+       {
+         $('#address').css('border', '5px solid green')
+         $('#submit').attr("disabled", false);
+       } else {
+         $('#address').css('border', '5px solid red')
+         $('#submit').attr("disabled", true);
+       }
+    }
+   });
+ 
+  });
 });
